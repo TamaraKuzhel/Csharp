d01d6e923e699635c1fd35c7484a48d68bc823fa
Tamara Kuzhel <tomikuzh@gmail.com>
2017-02-13 15:43:22 +0200

    Adding homework3, second try

--- Lesson3/MaxMinArray/MaxMinArray/Program.cs
+++ Lesson3/MaxMinArray/MaxMinArray/Program.cs
@@ -16,30 +16,48 @@ namespace CustomSizedArray
             //assigning value for each array item
             for (int i = 0; i < size; i++)
             {
-                Console.WriteLine("Please set value for element {0}", i+1);
+                Console.WriteLine("Please set value for element {0}", i + 1);
                 array[i] = Convert.ToInt32(Console.ReadLine());
-               
+
             }
 
-            int max = 0;
-            int min = array[0];
-
-            for (int i = 0; i < size; i++)
-            {
-                if (array[i] > max)
-                {
-                    max = array[i];
-                }
-                if (array[i] < min)
-                {
-                    min = array[i];
-                }
-            }
+            int max = DefineMax(array);
+            int min = DefineMin(array);
 
             Console.WriteLine("Min value in the array is {0}", min);
             Console.WriteLine("Max value in the array is {0}", max);
             Console.ReadKey();
 
         }
+
+        public static int DefineMax(int[] inPut)
+        {
+            int max = inPut[0];
+            for (int i = 0; i < inPut.Length; i++)
+            {
+                if (inPut[i] > max)
+                {
+                    max = inPut[i];
+                }
+            }
+            return max;
+        }
+
+        public static int DefineMin(int[] inPut)
+        {
+            int min = inPut[0];
+            for (int i = 0; i < inPut.Length; i++)
+            {
+                if (inPut[i] < min)
+                {
+                    min = inPut[i];
+                }
+            }
+            return min;
+        }
+
+           
+
+        
     }
 }
